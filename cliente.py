8fecfb9e6f2b568ef133a3910478b9f90e930ab0
Yago César <yagocdj@gmail.com>
2022-12-12 14:44:25 -0300

    Adicionando a lógica de conexão cliente/servidor

--- cliente.py
+++ cliente.py
@@ -1,31 +1,69 @@
+#!/usr/bin/env python3
+
 from pilhaEncadeada import Pilha
+import socket
+import sys
 
-tentativas = Pilha()
-palavras = ['batata', 'macaxeira', 'inhame']
+HOST = '127.0.0.1'
+PORT = 5000
 
-while len(palavras) != 0:  
-    print(f's {len(palavras)} {palavras}')    
-    print(tentativas)
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
 
-    # Extrair somente tentativa única
-    while True:
+nome_jogador = input('Insira o seu nome de usuário : ')
+
+servidor = (HOST, PORT)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(servidor)
+
+sock.send(str.encode(nome_jogador))
+retorno = sock.recv(1024)
+
+if retorno:
+    retorno = retorno.decode()
+    print(f"Recebi {retorno} do servidor")
+else:
+    print("Não há partidas")
+
+while True:
+    try:
+        msg = 'RYLP > '
+        msg +=  input('\nRYLP (Ctrl + D para encerrar) > ') # 'Oi Mundo Socket!' -> mensagem de teste
+        print(msg)
+    except EOFError: break
+    sock.send(str.encode(msg))  # encode vai retornar um vetor de bytes, já que o send espera receber um vetor de bytes
+    msg = sock.recv(1024)
+    if msg:
+        msg = msg.decode()
+        print('Recebi:', msg)
+sock.close()
+
+# tentativas = Pilha()
+# palavras = ['batata', 'macaxeira', 'inhame']
+
+# while len(palavras) != 0:  
+#     print(f's {len(palavras)} {palavras}')    
+#     print(tentativas)
+
+#     # Extrair somente tentativa única
+#     while True:
         
-        tentativa = input('Tentativa : ').lower().strip()
+#         tentativa = input('Tentativa : ').lower().strip()
 
-        #SEMÁFORO?
-        if not tentativas.existe(tentativa):
-            break
-        print('Tentativa repetida, tente novamente!')
+#         #SEMÁFORO?
+#         if not tentativas.existe(tentativa):
+#             break
+#         print('Tentativa repetida, tente novamente!')
     
-    # Verificar se tentativa é correta
-    if tentativa in palavras:
-        for i in range(len(palavras)):
-            if palavras[i] == tentativa:
-                palavras.pop(i)
-                break
-        print(f'O palpite da palavra {tentativa} de PESSOA estava certo!')
-    else:
-        print(f'O palpite da palavra {tentativa} de PESSOA estava errado...')
-    tentativas.empilha(tentativa)
+#     # Verificar se tentativa é correta
+#     if tentativa in palavras:
+#         for i in range(len(palavras)):
+#             if palavras[i] == tentativa:
+#                 palavras.pop(i)
+#                 break
+#         print(f'O palpite da palavra {tentativa} de PESSOA estava certo!')
+#     else:
+#         print(f'O palpite da palavra {tentativa} de PESSOA estava errado...')
+#     tentativas.empilha(tentativa)
 
-print('FIM DO PROGRAMA')
+# print('FIM DO PROGRAMA')
--- servidor.py
+++ servidor.py
@@ -1,27 +1,44 @@
+#!/usr/bin/env python3
+
 from arvoreBinaria import ArvoreBinaria
 import socket
 import threading
 
 TAM_MSG = 1024
 HOST = '0.0.0.0'
-PORT = '5000'
+PORT = 5000
 
 partida = ArvoreBinaria()
 
+################################
+## MÉTODOS DO NOSSO PROTOCOLO ##
+################################
+
+# palpite
+# ver tentativa
+# ver respostas
+# sair
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv = (HOST, PORT)
+sock.bind(serv)
+sock.listen(16)
+
+palavras = ['batata', 'macaxeira', 'inhame']
+clientes = []
+
 def processar_cliente(con, cliente):  # con -> socket de conexão; cliente -> IP: PORT do parceiro
     print('Conectado com', cliente)
+    # O RESTO
     while True:
-        msg = con.recv(1024)
+        msg = con.recv(TAM_MSG)
         if not msg: break
         print(cliente, 'mensagem:', msg.decode())
         con.send(msg)
     print('Desconectando do cliente', cliente)
     con.close()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv = (HOST, PORT)
-sock.bind(serv)
-sock.listen(50)
+
 while True:
     try:
         con, cliente = sock.accept()  # con -> socket retornado pelo accept
